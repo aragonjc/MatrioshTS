f92cd82383fa4982a1f8ef7a25dba80a4cac0b18
Juank Aragón <juank1316@gmail.com>
2020-11-12 21:17:40 -0600

    Genera un archivo de 3d

--- app.js
+++ app.js
@@ -10,25 +10,60 @@ try {
     return;
 }
 
+function check(element) {
+    if(element.constructor.name == "Function") {
+        return false;
+    } else if(element.constructor.name == "Variable"/* ||element.constructor.name == "VariableChange" */) {
+        return false;
+    } else if(element.constructor.name == "decType") {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 let Scope = new scope(null,0,0);
 let ast = gramatica.parse(entrada.toString());
 let code = '';
+code += '#include <stdio.h>\n\n';
 code += 'float P = 0;\n';
 code += 'float H = 0;\n';
-code += 'float Stack[100000];\n';
-code += 'float Heap[100000];\n'
+code += 'float Stack[16394];\n';
+code += 'float Heap[16384];\n'
 let terminals = 'float t1';
+
+let Globales = '';
+let Funciones = '';
+let Main = '\n\nvoid main(){\n';
+
+ast.forEach(element => {
+    if(element.constructor.name == "Variable" /*||element.constructor.name == "VariableChange"*/ ) {
+        //console.log(element)
+        let r = element.translate(Scope,null,null,null,null,null); 
+        Globales += r.code3d;
+    }
+})
+
+ast.forEach(element => {
+    if(element.constructor.name == "Function") {
+        let r = element.translate(Scope,null,null,null,null,null);
+        Funciones += r.code3d;
+    }
+})
 
 
 ast.forEach(element => {
-   let r = element.translate(Scope,null,null,null); 
-   code += r.code3d;
-   //console.log(r.code3d)
+    if(check(element)) {
+        let r = element.translate(Scope,null,null,null,null,null); 
+        Main += r.code3d;
+    }
 });
+Main += 'return;\n}';
+
 
 for(let i = 1;i<Scope.terminal;i++){
    terminals +=  ',t'+(i+1);
 }
-terminals += ';\n';
-let resultado = terminals + code;
+terminals += ';\n\n';
+let resultado = code +  terminals + Globales + Funciones + Main;
 console.log(resultado)
